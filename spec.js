c3d0e2fd2a672210643cc82d466744367316f9b8
Duarte Ferreira Dias <dudias@microsoft.com>
2020-06-03 22:55:00 +0100

    improved information logged in console

--- spec.js
+++ spec.js
@@ -5,6 +5,7 @@ describe('Instagram fast commenter', function() {
   // Edit the information bellow!
   // Target page
   const targetURL = 'https://www.instagram.com/myTargetPage';
+  // You need to change this information before running the script!
   var numberOfPostsOnTargetProfile = 69;
   const commentText = 'This is the comment I am posting';
   // My instagram user information
@@ -12,10 +13,12 @@ describe('Instagram fast commenter', function() {
   const myPassword = 'myPassword123';
   // Number of times the page should refresh
   // Each iteration takes around 2.5 seconds
+  // Recommended value: 30000
   // 30000 iterations take aproximately 21 hours to complete ((2.5*30000)/3600) = 20.88 hours
   var numberOfIterations = 30000;
 
   // Do not edit the information bellow!
+  var numberOfCommentsPosted = 0
 
   // This code runs once for loging into your account
   browser.driver.get('https://www.instagram.com/accounts/login/');
@@ -31,13 +34,17 @@ describe('Instagram fast commenter', function() {
   function instaAutoCommenter(numberOfIterations){
     it('should win me a ticket for that cool music festival', function() {
 
-      browser.driver.findElement(by.css('.g47SY')).getText().then( (data) => {
-        console.log(numberOfIterations);
-        console.log(data);
-        var auxNumberOfPosts = data.replace('.','');
-        auxNumberOfPosts = parseInt(auxNumberOfPosts, 10);
-        console.log(auxNumberOfPosts);
-        if(auxNumberOfPosts != numberOfPostsOnTargetProfile){
+      browser.driver.findElement(by.xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[1]/span/span')).getText().then( (numberOfPostsString) => {
+        // Log run information at each interation
+        console.log('\nCurrent iteration: ' + (30000 - numberOfIterations) + ' / 30 000');
+        console.log('Number of comments posted: ' + numberOfCommentsPosted);
+        console.log('Current number of posts on page: ' + numberOfPostsString);
+
+        // Convert value of posts on target profile from string to int
+        var numberOfPostsInt = numberOfPostsString.replace('.','');
+        numberOfPostsInt = parseInt(numberOfPostsInt, 10);
+
+        if(numberOfPostsInt != numberOfPostsOnTargetProfile){
           browser.driver.sleep(1500);
           browser.driver.findElement(by.css('._9AhH0')).click();
           browser.driver.sleep(1500);
@@ -47,6 +54,7 @@ describe('Instagram fast commenter', function() {
           browser.driver.findElement(by.css('.Ypffh')).sendKeys(protractor.Key.ENTER);
           browser.driver.sleep(2000);
           numberOfPostsOnTargetProfile++;
+          numberOfCommentsPosted++;
         }
         browser.driver.sleep(2500);
         browser.driver.get(targetURL);
